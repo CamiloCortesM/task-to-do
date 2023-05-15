16993bc7aa9b456930f09b82cac110fb231740cd
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-05-15 15:37:48 -0500

    stdin,stdout,readline and menu

---
+++ helpers/messages.js
@@ -0,0 +1,41 @@
+require("colors");
+
+const showMenu = () => {
+  console.clear();
+  console.log("========================".green);
+  console.log("   Select an option".green);
+  console.log("========================\n".green);
+
+  console.log(`${"1".green}. Create todo`);
+  console.log(`${"2".green}. List todos`);
+  console.log(`${"3".green}. List completed todos`);
+  console.log(`${"4".green}. List pending todos`);
+  console.log(`${"5".green}. Complete todo(s)`);
+  console.log(`${"6".green}. Delete todo`);
+  console.log(`${"0".green}. Exit \n`);
+
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  readline.question("Select an option: ", (opt) => {
+    readline.close();
+  });
+};
+
+const pause = () => {
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  readline.question(`\nPress ${"ENTER".green} to continuen\n`, (opt) => {
+    readline.close();
+  });
+};
+
+module.exports = {
+  showMenu,
+  pause,
+};
